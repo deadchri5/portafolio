5ae0e096bb453ab805406b75139bc0ddf71e5340
deadchri5 <deadchri5h@gmail.com>
2021-07-22 23:18:43 -0500

    nuevo boton agregado a las cartas

--- res/scripts/portfolio.js
+++ res/scripts/portfolio.js
@@ -7,37 +7,49 @@ const works = [
     desc:
       "Es una página web que tiene como finalidad ayudar a animales domésticos en " +
       "situación de calle a encontrar un hogar. Tecnologías usadas: (Angular, Node JS, MongoDB).",
-    link: "https://github.com/deadchri5",
+    repolink: "https://github.com/deadchri5/adoptaMe/tree/frontend",
+    medialink: "www.google.com",
+    isDeployed: false,
   },
   {
     image: "./res/images/Projects/projMinisuper-min.jpg",
     name: "E-commerce para tienda de abarrotes",
     desc: "Sistema de e-commerce desarrollado para una tienda local. Tecnologías (Angular, Laravel, y MySQL).",
-    link: "https://github.com/deadchri5/minisuper",
+    repolink: "https://github.com/deadchri5/minisuper",
+    medialink: "https://www.google.com",
+    isDeployed: false,
   },
   {
     image: "./res/images/Projects/java_interface2-min.png",
     name: "Herramienta de administración",
     desc: "Fui parte de este proyecto, es un software de escritorio el cual tiene la finalidad de ayudar a las cuentas en un restaurante, fue desarrollado con Java y FXML.",
-    link: "https://github.com/deadchri5",
+    repolink: "https://github.com/deadchri5",
+    medialink: "https://www.google.com",
+    isDeployed: false,
   },
   {
     image: "./res/images/Projects/projCham-min.jpg",
-    name: "Sitio web personal",
+    name: "Sitio web para establecer contacto",
     desc: "Sitio web para ponerse en contacto con un equipo de desarrollo de software. Este sitio fue creado con ReactJS como tecnología de frontend.",
-    link: "https://champrogrammers.github.io/web/",
+    repolink: "https://github.com/champrogrammers/web",
+    medialink: "https://champrogrammers.github.io/web/",
+    isDeployed: true,
   },
   {
     image: "./res/images/Projects/citasVet-min.jpg",
     name: "Sistema de citas",
     desc: "Es un proyecto escolar para gestionar y agendar citas en una veterinaria. Escrito en C# y WPF.",
-    link: "https://github.com/deadchri5/VeterinariaCS",
+    repolink: "https://github.com/deadchri5/VeterinariaCS",
+    medialink: "https://www.google.com",
+    isDeployed: false,
   },
   {
     image: "",
     name: "Código de portafolio",
     desc: "Código de mi portafolio personal, si usted lo desea observar pulse en el botón de la tarjeta para ver el repositorio.",
-    link: "https://github.com/deadchri5/portafolio/tree/master",
+    repolink: "https://github.com/deadchri5/portafolio/tree/master",
+    medialink: "https://trusting-mirzakhani-aef1aa.netlify.app/",
+    isDeployed: true,
   },
 ];
 
@@ -46,15 +58,16 @@ const setWorkinCards = () => {
   let i = 0;
   works.forEach((work) => {
     let cardEffect;
+    let mediaButtonText;
     if (i % 2 == 0)
       cardEffect =
         'data-aos="flip-left" data-aos-delay="50" data-aos-duration="1000"';
     else
       cardEffect =
         'data-aos="flip-right" data-aos-delay="50" data-aos-duration="1000"';
-    console.log(work.image);
     if (work.image == "") work.image = "./res/images/Projects/picture-min.jpg";
-
+    if (work.isDeployed) mediaButtonText = "Ver trabajo";
+    else mediaButtonText = "Ver imágenes";
     cardContainer.innerHTML +=
       '<div class="card"' +
       cardEffect +
@@ -72,8 +85,13 @@ const setWorkinCards = () => {
       work.desc +
       "</p>" +
       '<a href="' +
-      work.link +
-      '" target="_blank" class="card__body__btn">Ver trabajo</a>' +
+      work.medialink +
+      '" target="_blank" class="card__body__btn card__body__btn__media">' +
+      mediaButtonText +
+      "</a>" +
+      '<a href="' +
+      work.repolink +
+      '" target="_blank" class="card__body__btn">Ver repositorio</a>' +
       "</div>" +
       "</div>";
     i++;
