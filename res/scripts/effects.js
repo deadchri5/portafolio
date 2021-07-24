1b029b4afd53f25e9b343a80647ef4733a2e82ae
deadchri5 <deadchri5h@gmail.com>
2021-07-23 23:45:44 -0500

    cambio

--- res/scripts/effects.js
+++ res/scripts/effects.js
@@ -33,10 +33,11 @@ document
 
 //Efect of slide sown element
 let slideDown = document.getElementById("slide-down");
-window.addEventListener('scroll', () => {
-  if (document.body.scrollTop >= 159 || document.documentElement.scrollTop >= 159) {
+window.addEventListener("scroll", () => {
+  if (
+    document.body.scrollTop >= 159 ||
+    document.documentElement.scrollTop >= 159
+  ) {
     slideDown.classList.add("slide-fade");
-  }
-  else
-  slideDown.classList.remove("slide-fade")
-})
+  } else slideDown.classList.remove("slide-fade");
+});
