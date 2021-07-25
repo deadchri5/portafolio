6dd43ce6ddeff8ed33921279e2c36bbc5e42da60
deadchri5 <deadchri5h@gmail.com>
2021-07-24 20:51:49 -0500

    menu movil terminado

--- res/scripts/buttons.js
+++ res/scripts/buttons.js
@@ -1,22 +1,31 @@
 //Global
-const aboutmeSection = document.getElementById('about-me');
-const contactSection = document.getElementById('particles-js');
-const portfolioSection = document.getElementById('portfolio');
+const aboutmeSection = document.getElementById("about-me");
+const contactSection = document.getElementById("particles-js");
+const portfolioSection = document.getElementById("portfolio");
 
-function gotoAboutSection(event){
-    event.preventDefault();
-    document.scrollTop = aboutmeSection.offsetTop - 80;
-    document.documentElement.scrollTop = aboutmeSection.offsetTop - 80;
+function gotoAboutSection(event) {
+  event.preventDefault();
+  document.scrollTop = aboutmeSection.offsetTop - 80;
+  document.documentElement.scrollTop = aboutmeSection.offsetTop - 80;
+  mobileMenu.classList.toggle("open-anim");
+  mobileNav.classList.toggle("remove-opacity");
+  hamburguer.classList.toggle("close");
 }
 
-function gotoContactSection(event){
-    event.preventDefault();
-    document.scrollTop = contactSection.offsetTop;
-    document.documentElement.scrollTop = contactSection.offsetTop;
+function gotoContactSection(event) {
+  event.preventDefault();
+  document.scrollTop = contactSection.offsetTop;
+  document.documentElement.scrollTop = contactSection.offsetTop;
+  mobileMenu.classList.toggle("open-anim");
+  mobileNav.classList.toggle("remove-opacity");
+  hamburguer.classList.toggle("close");
 }
 
-function gotoPortfolioSection(event){
-    event.preventDefault();
-    document.scrollTop = portfolioSection.offsetTop;
-    document.documentElement.scrollTop = portfolioSection.offsetTop;
+function gotoPortfolioSection(event) {
+  event.preventDefault();
+  document.scrollTop = portfolioSection.offsetTop;
+  document.documentElement.scrollTop = portfolioSection.offsetTop;
+  mobileMenu.classList.toggle("open-anim");
+  mobileNav.classList.toggle("remove-opacity");
+  hamburguer.classList.toggle("close");
 }
--- res/scripts/navbarMobile.js
+++ res/scripts/navbarMobile.js
@@ -1,9 +1,9 @@
-const mobileNav = document.getElementById('nav-mobile');
-const hamburguer = document.querySelector('.hamburguer');
-const mobileMenu = document.querySelector('.mobile-menu');
+const mobileNav = document.getElementById("nav-mobile");
+const hamburguer = document.querySelector(".hamburguer");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-hamburguer.addEventListener('click', () => {
-    mobileMenu.classList.toggle('open-anim');
-    mobileNav.classList.toggle('remove-opacity');
-    hamburguer.classList.toggle('close');
-})
+hamburguer.addEventListener("click", () => {
+  mobileMenu.classList.toggle("open-anim");
+  mobileNav.classList.toggle("remove-opacity");
+  hamburguer.classList.toggle("close");
+});
